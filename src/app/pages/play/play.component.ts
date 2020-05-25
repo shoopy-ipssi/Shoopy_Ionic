f9b869f2df1ae80fae10665571bd55e36c4b1d6d
Sacar <antoine95560@hotmail.fr>
2020-05-25 11:49:18 +0200

    call mongo

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -3,9 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { VariablesGlobales  } from '../../variables-globales/variables-globales.component';
 import { Router } from '@angular/router';
+import * as $ from 'jquery'
 
 const UID = sessionStorage.getItem('UID')
 const data = { params: {userID: UID}} 
+
 
 @Component({
   selector: 'app-play',
@@ -14,15 +16,35 @@ const data = { params: {userID: UID}}
 })
 export class PlayComponent implements OnInit {
   id: any;
+  scenario: any;
 
   constructor( private route: ActivatedRoute, private http: HttpClient, public gv: VariablesGlobales, private router: Router) {
     }
+    
+    typeTextAnimated(sentence) {
+      var ele = '<span>' + sentence.split('').join('</span><span>') + '</span>';
+      
+      
+      $(ele).hide().appendTo('#block_att').each(function (i) {
+          $(this).delay(100 * i).css({
+              display: 'inline',
+              opacity: 0
+          }).animate({
+              opacity: 1
+          }, 100);
+      });
+      }
+      private text = ""
     private user = []
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
-    return this.http.get(`${this.gv.apiUrl}user`, data).subscribe(results => {
+    this.http.get(`${this.gv.apiUrl}user`, data).subscribe(results => {
       this.user = results[0]; 
-    })
+    });
+    this.http.get(`${this.gv.mongUrl}scenario`).subscribe((res: Response) => { this.scenario = res 
+      console.log(this.scenario)}
+    );
+    this.typeTextAnimated(this.text)
   }
 
 
