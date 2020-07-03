2abcbe577b5f2b7a3735556f0a2ff67f451ffcca
GenoudJeremy <genoud.jeremy@outlook.com>
2020-07-03 09:14:21 +0200

    Add Registration Verification

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EncrDecrService } from 'src/app/services/crypto.service';
 import { VariablesGlobalesComponent } from 'src/app/variables-globales/variables-globales.component';
+import { data } from 'jquery';
 
 
 @Component({
@@ -16,12 +17,14 @@ export class RegisterComponent implements OnInit {
   public session: Response;
   public log: any;
   public sendMail: boolean
+  public emailAlready: boolean
+  public usernameAlready: boolean
   constructor (public http: HttpClient, public fb: FormBuilder, public router: Router, public EncrDecr: EncrDecrService, public gv: VariablesGlobalesComponent) {
     this.sendMail = false
   }
   ngOnInit() {
     this.RegisterData = this.fb.group({
-      email: ['', Validators.required], // la chaîne de caractères est la valeur par défaut du champ
+      email: ['', [Validators.required, Validators.email]], // la chaîne de caractères est la valeur par défaut du champ
       password: ['', Validators.required],
       lastname: ['', Validators.required],
       firstname: ['', Validators.required],
@@ -29,25 +32,6 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  async Validate() {
-    const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    if (this.RegisterData.value.email.match(emailRegex)) {
-      const decrypted = this.RegisterData.value.password
-      if (decrypted == this.log[0].password) {
-        const email = sessionStorage.setItem('email', this.log[0].email);
-        sessionStorage.setItem('UID', this.log[0].id)
-      } else {
-        alert('Mot de passe incorrect')
-      }
-    } else {
-      alert('Email incorrect')
-    }
-    this.gv.changelsLog('true')
-    if (this.gv.getisLoggedIn()){
-      this.gv.getUser()
-      this.router.navigate(['']);
-    }
-  }
   sendMailRegister(idU){
     const dataForm = this.RegisterData.value
     const id = idU
@@ -61,29 +45,36 @@ export class RegisterComponent implements OnInit {
     })
   }
   async Register() {
-    const datas = this.RegisterData.value;
-    datas.password = this.EncrDecr.set(this.gv.cryptVal, datas.password)
-    this.http.post(`${this.gv.apiUrl}createuser`, datas, { headers: this.gv.headers }).
-      subscribe((res: Response) => {
-        this.log = res;
-        if (this.log !== undefined) {
-          this.sendMailRegister(this.log.insertId)
-          //this.Signin()
+    let data_res:any
+    if (this.RegisterData.status == 'VALID'){
+      const datas = this.RegisterData.value;
+      datas.password = this.EncrDecr.set(this.gv.cryptVal, datas.password)
+      this.http.post(`${this.gv.apiUrl}checkuser`, {username: datas.username, email: datas.email}, {headers: this.gv.headers}).subscribe(res=>{
+        data_res = res[0]
+        if (data_res.username > 0){
+          this.usernameAlready = true
         } else {
-          this.router.navigate(['']);
+          this.usernameAlready = false
         }
-      });
-  }
-  async Signin() {
-    const datas = this.RegisterData.value;
-    this.http.post(`${this.gv.apiUrl}login`, datas, { headers: this.gv.headers }).
-      subscribe((res: Response) => {
-        this.log = res;
-        if (this.log !== undefined) {
-          this.Validate();
+        if (data_res.email > 0){
+          this.emailAlready = true
         } else {
-          this.router.navigate(['']);
+          this.emailAlready = false
         }
-      });
+        if (!this.emailAlready && !this.usernameAlready){
+          this.http.post(`${this.gv.apiUrl}createuser`, datas, { headers: this.gv.headers }).subscribe((res: Response) => {
+            this.log = res;
+            if (this.log !== undefined) {
+              this.sendMailRegister(this.log.insertId)
+              //this.Signin()
+            } else {
+              this.router.navigate(['']);
+            }
+          });
+        }
+      })
+    } else {
+      alert('Merci de remplir tous les champs')
+    }   
   }
 }
